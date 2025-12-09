65950fe906f2152219c441b74d69b0842d9b1cc7
Missael Rangel <missael.rangeel@gmail.com>
2025-12-09 15:55:20 -0600

    buscador listo

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(["goku"]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = (term: string) => {
     console.log(term);
@@ -23,7 +24,7 @@ export const GifsApp = () => {
     setPreviousTerms([query, ...previousTerms].slice(0, 8));
 
     const gifs = await getGifsByQuery(query);
-    console.log(gifs);
+    setGifs(gifs);
   };
 
   return (
@@ -44,7 +45,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
--- src/gifs/actions/get-gifs-by-query.action.ts
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,19 +1,14 @@
-import axios from "axios";
 import type { GiphyResponse } from "../interfaces/giphy.response";
 import type { Gif } from "../interfaces/gif.interface";
+import { giphyApi } from "../api/giphy.api";
 
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-  const response = await axios.get<GiphyResponse>(
-    "https://api.giphy.com/v1/gifs/search",
-    {
-      params: {
-        q: query,
-        limit: 10,
-        lang: "es",
-        api_key: "e2UmgszTQWNicWRaqjkpeV3FNk6eBRiF",
-      },
-    }
-  );
+  const response = await giphyApi<GiphyResponse>("/search", {
+    params: {
+      q: query,
+      limit: 10,
+    },
+  });
   return response.data.data.map((gif) => ({
     id: gif.id,
     title: gif.title,
