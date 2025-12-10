b53c1e6411916b5abfa9c45ca655435a0424dd9d
Missael Rangel <missael.rangeel@gmail.com>
2025-12-09 18:12:11 -0600

    Custom hook hecho

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { GifsApp } from "./GifsApp";
+// import { GifsApp } from "./GifsApp";
+import { MyCounterApp } from "./counter/components/MyCounterApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GifsApp />
+    {/* <GifsApp /> */}
+    <MyCounterApp />
   </StrictMode>
 );
